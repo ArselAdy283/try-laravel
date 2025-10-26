9140144741adf32723689e776dcf0c02057c62fd
egaadyatma <egaadyatmapuspito@gmail.com>
2025-10-26 17:59:00 +0700

    belajar mengelola file di laravel

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Ega Adyatma",
+        "edu" => "SMEA"
+    ]);
+});
+
