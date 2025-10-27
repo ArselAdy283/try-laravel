be2bb679eb803b22ef65363a5d34ce9bbc8ffde0
egaadyatma <egaadyatmapuspito@gmail.com>
2025-10-28 00:39:31 +0700

    menambahkan filament

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -113,7 +113,7 @@
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
-                <h1>INI ABOUT</h1>
+                <h1>INI HOME</h1>
             </div>
         </main>
     </div>
